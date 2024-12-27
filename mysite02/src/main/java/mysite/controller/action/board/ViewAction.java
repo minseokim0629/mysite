a38518c792971f8685e39b3e2edd1133d78c71c6
minseokim0629 <han8121449@naver.com>
2024-12-27 16:30:48 +0900

    241227 게시판 과제 수정 (조회수 쿠키로 관리)

--- mysite02/src/main/java/mysite/controller/action/board/ViewAction.java
+++ mysite02/src/main/java/mysite/controller/action/board/ViewAction.java
@@ -1,69 +1,52 @@
 package mysite.controller.action.board;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import mysite.controller.ActionServlet.Action;
 import mysite.dao.BoardDao;
 import mysite.vo.BoardVo;
-import mysite.vo.UserVo;
 
 public class ViewAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// HttpSession session = request.getSession();
 		
-//		Long id = Long.parseLong(request.getParameter("id"));
-//		UserVo authUser = (UserVo)session.getAttribute("authUser");
-//		Long userId = 0L;
-//		if(authUser != null) {
-//			userId = authUser.getId();
-//		}
-//		// 쿠키 읽기
-//		Cookie[] cookies = request.getCookies();
-//		boolean isViewed = false;
-//		String existingValue = "";
-//		String newEntry = id + ","+ userId;
-//		for(Cookie cookie : cookies) {
-//			if(cookie.getName().equals("viewed")) {
-//				existingValue = cookie.getValue();
-//				if(existingValue.contains(id + "," + userId)) {
-//					isViewed = true;
-//					break;
-//				}
-//			}
-//		}
-//		
-//		if (!existingValue.contains(newEntry)) {
-//	        if (!existingValue.isEmpty()) {
-//	            existingValue += "|"; // 기존 값과 구분자를 추가
-//	        }
-//	        existingValue += newEntry;
-//		}
-//		
-//		BoardVo vo = new BoardDao().findById(id);
-//		request.setAttribute("vo", vo);
-//		
-//		if(!isViewed) {
-//			new BoardDao().updateHit(vo);
-//			// 쿠키 생성
-//			Cookie cookie = new Cookie("viewed", existingValue);
-//			cookie.setPath(request.getContextPath());
-//			cookie.setMaxAge(60 * 60 * 24); // max : 1 day
-//			response.addCookie(cookie);
-//		}
 		Long id = Long.parseLong(request.getParameter("id"));
+
+		// 쿠키 읽기
+		Cookie[] cookies = request.getCookies();
+		boolean isViewed = false;
+		String existingValue = "";
+		for(Cookie cookie : cookies) {
+			if(cookie.getName().equals("viewed")) {
+				existingValue = cookie.getValue();
+				if(List.of(existingValue.split(":")).contains(id.toString())){
+					System.out.println(existingValue);
+					isViewed = true;
+					break;
+				}
+			}
+		}
 		
 		BoardVo vo = new BoardDao().findById(id);
 		request.setAttribute("vo", vo);
 		
-		new BoardDao().updateHit(vo);
+		if(!isViewed) {
+			new BoardDao().updateHit(vo);
+			// 쿠키 생성
+			Cookie cookie = new Cookie("viewed", existingValue + id + ":");
+			cookie.setPath(request.getContextPath());
+			cookie.setMaxAge(60 * 60 * 24); // max : 1 day
+			System.out.println(cookie.getValue());
+			response.addCookie(cookie);
+		}
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/board/view.jsp");
 		rd.forward(request, response);
