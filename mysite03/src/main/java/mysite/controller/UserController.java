31b97d4c4eb078679297aed20996893bd8e47327
minseokim0629 <han8121449@naver.com>
2024-12-30 17:06:26 +0900

    241230_3 session으로 유저 반환하는 로직 분리 (Annotation & ArgumentResolver)

--- mysite03/src/main/java/mysite/controller/UserController.java
+++ mysite03/src/main/java/mysite/controller/UserController.java
@@ -5,7 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import jakarta.servlet.http.HttpSession;
+import mysite.security.Auth;
+import mysite.security.AuthUser;
 import mysite.service.UserService;
 import mysite.vo.UserVo;
 
@@ -41,55 +42,59 @@ public class UserController {
 		return "user/login";
 	}
 	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(HttpSession session, UserVo userVo, Model model) {
-		UserVo authUser = userService.getUser(userVo.getEmail(), userVo.getPassword());
-		// 로그인 실패
-		if(authUser == null) {
-			model.addAttribute("email", userVo.getEmail());
-			model.addAttribute("result", "fail");
-			return "user/login";
-		}
-		
-		// login 처리
-		session.setAttribute("authUser", authUser);
-		return "redirect:/";
-	}
+//	@RequestMapping(value = "/login", method = RequestMethod.POST)
+//	public String login(HttpSession session, UserVo userVo, Model model) {
+//		UserVo authUser = userService.getUser(userVo.getEmail(), userVo.getPassword());
+//		// 로그인 실패
+//		if(authUser == null) {
+//			model.addAttribute("email", userVo.getEmail());
+//			model.addAttribute("result", "fail");
+//			return "user/login";
+//		}
+//		
+//		// login 처리
+//		session.setAttribute("authUser", authUser);
+//		return "redirect:/";
+//	}
 	
-	@RequestMapping("/logout")
-	public String logout(HttpSession session) {
-		session.removeAttribute("authUser");
-		session.invalidate();
-		
-		return "redirect:/";
-	}
+//	@RequestMapping("/logout")
+//	public String logout(HttpSession session) {
+//		session.removeAttribute("authUser");
+//		session.invalidate();
+//		
+//		return "redirect:/";
+//	}
 	
+	@Auth
 	@RequestMapping(value = "/update", method = RequestMethod.GET)
-	public String update(HttpSession session, Model model) {
-		// Access Control
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
+	public String update(@AuthUser UserVo authUser, Model model) {
+		// Access Control	
+//		if(authUser == null) {
+//			return "redirect:/";
+//		}		
+		
+//		UserVo authUser = (UserVo)session.getAttribute("authUser");
+		
 		UserVo userVo = userService.getUser(authUser.getId());
 		
 		model.addAttribute("vo", userVo);
 		return "user/update";
 	}
 	
+	@Auth
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String update(HttpSession session, UserVo userVo) {
-		// Access Control
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
+	public String update(@AuthUser UserVo authUser, UserVo userVo) {
+		// Access Control		
+//		if(authUser == null) {
+//			return "redirect:/";
+//		}
+//		
+//		UserVo authUser = (UserVo)session.getAttribute("authUser");
 		
 		userVo.setId(authUser.getId());
 		userService.update(userVo);
 
 		authUser.setName(userVo.getName());
-		System.out.println();
 		return "redirect:/user/update";
 	}
 }
