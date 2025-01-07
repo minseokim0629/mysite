6338a8894fb2b1c05faaeaba40ccb58bef92b5f6
minseokim0629 <han8121449@naver.com>
2025-01-07 11:45:56 +0900

    250107_1 title 관리 ApplicationContextEventListener

--- mysite03/src/main/java/mysite/event/ApplicationContextEventListener.java
+++ mysite03/src/main/java/mysite/event/ApplicationContextEventListener.java
@@ -1,11 +1,15 @@
 package mysite.event;
 
+import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 
 import mysite.service.SiteService;
+import mysite.vo.SiteVo;
 
 public class ApplicationContextEventListener {
 	@Autowired
@@ -13,8 +17,24 @@ public class ApplicationContextEventListener {
 
 	@EventListener({ContextRefreshedEvent.class})
 	public void handlerContextRefreshEvent() {
+		System.out.println("-- Context Refreshed Event Received --");
+		
 		SiteService siteService = applicationContext.getBean(SiteService.class);
-		System.out.println("-- Context Refreshed Event Received --" + siteService);
+		SiteVo vo = siteService.getSite();
+		
+		//site를 bean으로 등록하는 작업
+		MutablePropertyValues propertyValues = new MutablePropertyValues();
+		propertyValues.add("title", vo.getTitle());
+		propertyValues.add("welcome", vo.getWelcome());
+		propertyValues.add("description", vo.getDescription());
+		propertyValues.add("profile", vo.getProfile());
+		
+		GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+		beanDefinition.setBeanClass(SiteVo.class);
+		beanDefinition.setPropertyValues(propertyValues);
+		
+		BeanDefinitionRegistry registry = (BeanDefinitionRegistry)applicationContext.getAutowireCapableBeanFactory();
+		registry.registerBeanDefinition("site", beanDefinition);
 		// 구현한 siteVo를? bean으로 등록하는 작업 및 jsp로 넘겨주기
 		//viewResolver에게 container에 잇는 걸 어떻게 노출시키냐?
 	}
