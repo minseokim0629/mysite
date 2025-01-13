535516a6da1318ce95fe7f36d6a4f4d93fde9094
minseokim0629 <han8121449@naver.com>
2025-01-13 09:46:59 +0900

    250113 Exception Handler 수정 (JSON과 HTML 요청 분기 처리)

--- mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
@@ -9,20 +9,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	private static final Log log = LogFactory.getLog(GlobalExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
-	public String handler(Model model, Exception e) {
+	public void handler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception{
 		// 1. 로깅(logging)
 		// StringWriter - 주스트림 PrintWriter - 보조스트림
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		log.error(errors.toString());
 		
-		// 2. 사과 페이지(종료)
-		model.addAttribute("errors", errors.toString());
-		return "errors/exception";
+		// 2. 요청 구분 
+		//	  json 요청 : request header의 accept : application/json 이 포함되어있음
+		//	  html 요청 : request header의 accept : application/json 이 포함되어 있지 않음
+		String accept = request.getHeader("accept");
+		
+		if(accept.matches(".*application.json.*")) {
+			// 3. JSON 응답
+		} else {
+			// 4. 사과 페이지(종료)
+			request.setAttribute("errors", errors.toString());
+			request.getRequestDispatcher("/WEB-INF/views/errors/exception.jsp").forward(request, response);
+		}
 	}
 }
