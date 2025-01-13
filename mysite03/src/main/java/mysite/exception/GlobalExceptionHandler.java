01dd522b7249ec71938f9f0d18a6adb22ad03f85
minseokim0629 <han8121449@naver.com>
2025-01-13 11:25:14 +0900

    250113 Exception Handler 수정 (JSON과 HTML 요청 분기 처리)

--- mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
@@ -1,16 +1,19 @@
 package mysite.exception;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mysite.dto.JsonResult;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -31,6 +34,14 @@ public class GlobalExceptionHandler {
 		
 		if(accept.matches(".*application.json.*")) {
 			// 3. JSON 응답
+			JsonResult jsonResult = JsonResult.fail(errors.toString());
+			String jsonString = new ObjectMapper().writeValueAsString(jsonResult);
+			
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.setContentType("application/json; charset=utf-8");
+			OutputStream os = response.getOutputStream();
+			os.write(jsonString.getBytes("utf-8"));
+			os.close();
 		} else {
 			// 4. 사과 페이지(종료)
 			request.setAttribute("errors", errors.toString());
